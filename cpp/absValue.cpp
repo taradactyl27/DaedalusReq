734353a7c722f4b7a68266196d5402a4718a1bfb
unknown <ataradachuk@gmail.com>
2018-08-26 20:41:25 -0400

    Small update

--- cpp/absValue.cpp
+++ cpp/absValue.cpp
@@ -4,14 +4,14 @@ int main()
 	int number;
 	int abs_number;
 
-	cout << "This program finds the absolute value of an integer." << endl;
-	cout << "Enter an integer (positive or negative): ";
-	cin >> number;
+	std::cout << "This program finds the absolute value of an integer." << endl;
+	std::cout << "Enter an integer (positive or negative): ";
+	std::cin >> number;
 	
 	// Calling the function Abs()
 	abs_number = Abs(number);
-	cout << "The absolute value of " << number << " is " << abs_number;
-	cout << endl;
+	std::cout << "The absolute value of " << number << " is " << abs_number;
+	std::cout << endl;
 	return 0;
 }
 // Function definition
